904a5ac3d5af8ab0d89391a2fcb51ce0e2b79b97
Sanderson Sa <sanderson@redhat.com>
2024-04-11 13:18:41 -0300

    init commit 2

--- src/main/java/xyz/sandersonsa/springbootcamelfile/SpringbootCamelFileApplication.java
+++ src/main/java/xyz/sandersonsa/springbootcamelfile/SpringbootCamelFileApplication.java
@@ -3,6 +3,9 @@ package xyz.sandersonsa.springbootcamelfile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import io.dekorate.openshift.annotation.OpenshiftApplication;
+
+// @OpenshiftApplication(replicas=2)
 @SpringBootApplication
 public class SpringbootCamelFileApplication {
 
